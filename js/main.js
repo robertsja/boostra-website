80e26f7d751baa1037c205ad9864e1731f866f4d
robertsja <jarrod.j.roberts@gmail.com>
2025-08-24 17:49:00 +1200

    Added border highlights to product suite buttons when clicked

--- js/main.js
+++ js/main.js
@@ -1,15 +1,19 @@
 function showLeadShowcase() {
     document.getElementById('lead-showcase').classList.add('active');
+    document.getElementById('lead-card').classList.add('border-highlight');
     // Scroll to the product showcase section
     document.getElementById('lead-showcase').scrollIntoView({behavior: 'smooth'});
 
     document.getElementById('support-showcase').classList.remove('active');
+    document.getElementById('support-card').classList.remove('border-highlight');
     document.getElementById('custom-showcase').classList.remove('active');
-    
+    document.getElementById('custom-card').classList.remove('border-highlight');
+
 }
 
 function hideLeadShowcase() {
     document.getElementById('lead-showcase').classList.remove('active');
+    document.getElementById('lead-card').classList.remove('border-highlight');
     // Scroll back to the services section
     document.getElementById('services').scrollIntoView({behavior: 'smooth'});
 }
@@ -17,16 +21,20 @@ function hideLeadShowcase() {
 
 function showSupportShowcase() {
     document.getElementById('support-showcase').classList.add('active');
+    document.getElementById('support-card').classList.add('border-highlight');
     // Scroll to the product showcase section
     document.getElementById('support-showcase').scrollIntoView({behavior: 'smooth'});
 
     document.getElementById('lead-showcase').classList.remove('active');
+    document.getElementById('lead-card').classList.remove('border-highlight');
     document.getElementById('custom-showcase').classList.remove('active');
-    
+    document.getElementById('custom-card').classList.remove('border-highlight');
+
 }
 
 function hideSupportShowcase() {
     document.getElementById('support-showcase').classList.remove('active');
+    document.getElementById('support-card').classList.remove('border-highlight');
     // Scroll back to the services section
     document.getElementById('services').scrollIntoView({behavior: 'smooth'});
 }
@@ -34,17 +42,20 @@ function hideSupportShowcase() {
 
 function showCustomShowcase() {
     document.getElementById('custom-showcase').classList.add('active');
+    document.getElementById('custom-card').classList.add('border-highlight');
     // Scroll to the product showcase section
     document.getElementById('custom-showcase').scrollIntoView({behavior: 'smooth'});
 
     document.getElementById('lead-showcase').classList.remove('active');
+    document.getElementById('lead-card').classList.remove('border-highlight');
     document.getElementById('support-showcase').classList.remove('active');
+    document.getElementById('support-card').classList.remove('border-highlight');
     
 }
 
 function hideCustomShowcase() {
     document.getElementById('custom-showcase').classList.remove('active');
+    document.getElementById('custom-card').classList.remove('border-highlight');
     // Scroll back to the services section
     document.getElementById('services').scrollIntoView({behavior: 'smooth'});
 }
-
